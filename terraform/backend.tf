a5d4396e62d3ffe9ebf78fc2a10e8f978d156f5d
kalu <kaluifekwe6@gmail.com>
2026-04-22 17:00:18 +0500

    Use new storage account tfstaterxnuj8ib

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 ﻿terraform {
   backend "azurerm" {
     resource_group_name  = "kalu_RG"
-    storage_account_name = "tfstate8bpa061m"
+    storage_account_name = "tfstaterxnuj8ib"
     container_name       = "tfstate"
     key                  = "epicbook.tfstate"
     use_azuread_auth     = true
